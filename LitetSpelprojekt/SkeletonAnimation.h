1ba5506e1f4f4fa9d3118d1eb52785a7700c06bd
johandersson1 <johan_bst@hotmail.com>
2021-04-13 09:51:36 +0200

    new

--- LitetSpelprojekt/SkeletonAnimation.h
+++ LitetSpelprojekt/SkeletonAnimation.h
@@ -1,15 +1,23 @@
 #pragma once
 #include <DirectXMath.h>
 #include <d3d11.h>
+#include "Windowsnumerics.h"
 
 using namespace DirectX;
+using namespace Windows::Foundation::Numerics;
 
-struct Skeleton {
-	static const int BONE_COUNT = 50;		// Ska vara
+static const int BONE_COUNT = 50;		// Ska vara hur många ben som finns med i skelettet
+
+struct Skeleton 
+{
 
 	int parentArr[BONE_COUNT];				// Parent of each bone
-	XMFLOAT4X4 invBindTxArr[BONE_COUNT];	// Inverse of original model transform in bind pose
-	XMFLOAT4X4 modelTxArr[BONE_COUNT];	    // (dynamic) Current model transform
-	XMFLOAT4X4 localTxArr[BONE_COUNT];	    // (dynamic) Current local transform
-	XMFLOAT4X4 finalTxArr[BONE_COUNT];		// (dynamic) What to upload to GPU
+	float4x4 invBindTxArr[BONE_COUNT];	// Inverse of original model transform in bind pose
+	float4x4 modelTxArr[BONE_COUNT];	    // (dynamic) Current model transform
+	float4x4 localTxArr[BONE_COUNT];	    // (dynamic) Current local transform
+	float4x4 finalTxArr[BONE_COUNT];		// (dynamic) What to upload to GPU
+
+	void startUp();
+	void perFrame();
+
 };

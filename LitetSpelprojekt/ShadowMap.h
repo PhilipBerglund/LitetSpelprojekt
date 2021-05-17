2d59b4590fab085b29509f934b567aad9aa78578
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2021-05-17 10:26:58 +0200

    Restored

--- LitetSpelprojekt/ShadowMap.h
+++ LitetSpelprojekt/ShadowMap.h
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <d3d11.h>
 #include <directxmath.h>
+#include "Graphics.h"
 
 
 class ShadowMap
@@ -9,15 +10,16 @@ class ShadowMap
 public:
 	ShadowMap(UINT width, UINT height);
 	~ShadowMap();
-	ID3D11ShaderResourceView* DepthMapSRV();
+	ComPtr<ID3D11ShaderResourceView> DepthMapSRV();
 	void BindDsvAndSetNullRenderTarget();
+	void UpdateLightAndShadow(Light& light);
 private:
 	ShadowMap(const ShadowMap& rhs);
 	ShadowMap& operator=(const ShadowMap& rhs);
 private:
 	UINT mWidth;
 	UINT mHeight;
-	ID3D11ShaderResourceView* mDepthMapSRV;
-	ID3D11DepthStencilView* mDepthMapDSV;
+	ComPtr<ID3D11ShaderResourceView> mDepthMapSRV;
+	ComPtr<ID3D11DepthStencilView> mDepthMapDSV;
 	D3D11_VIEWPORT mViewport;
 };
